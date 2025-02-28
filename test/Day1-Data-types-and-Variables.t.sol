af37b45b1399f3e49e1c736f8668330c2ee0eb6a
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-02-28 12:56:18 +0530

    fixing the day1 test file

--- test/Day1-Data-types-and-Variables.t.sol
+++ test/Day1-Data-types-and-Variables.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
 import "../src/Day1-Data-types-and-Variables.sol";
 
 contract Day1_Data_types_and_Variables is Test {
@@ -11,13 +12,23 @@ contract Day1_Data_types_and_Variables is Test {
         dataTypes = new DayOne();
     }
 
-    /*    "view" If we are not modifying the blockchain then we can use view other-wise it may give an error "Function state mutability can be restricted to view"  
+    /* 
+    "view" If we are not modifying the blockchain then we can use view other-wise it may give an error "Function state mutability can be restricted to view"  
     
     Benefits -->
     1. function marked as view do not consume gas If we don't use it solidity may assume the function may modify the blockchain
     2. It helps in the clear semantic.
 */
+
+    /* 
+    console.log() -->
+    It used to debug the smart contract it used to print the statement like javascript. 
+    It allow to print the value inside the test function
+
+*/
     function testPostiveNumber() public view {
-        assertEq(dataTypes.postiveNumber(), 42);
+        uint256 number = dataTypes.postiveNumber();
+        console.log("Positive Number:", number);
+        assertEq(number, 42);
     }
 }
