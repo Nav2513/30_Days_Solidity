e978757d8ba007c06cabb56994c5829956a393f3
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-03-13 10:45:52 +0530

    Adding Day ten material fundamental of blockchain

---
+++ src/Day-ten-Ethereum-and-Blockchain-fundamental.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/* 
+ 
+        --- Ethereum & Blockchain Fundamental ---
+
+    Ethereum Virtual Machine
+
+The Ethereum Virtual Machine (EVM) is the decentralised computing engine that powers Ethereum. It acts as a runtime environment for executing smart contracts and decentralized application (dApp) on the Ethereum Blockchain.
+
+The Ethereum Virtual Machine(EVM) is like Java Virtual Machine(JVM). but ut has a diffrenet focus. Both are virtual machines that let program run on the diffrent systems without needing changes.
+
+Key Feature of EVM 
+
+1. Turing complete: It can execute any algorithm, given enough time and resources(gas).
+
+2. Sandboxed Environment: Code runs in isolation from the host system, preventing unauthorized access.
+
+3. Gas Mechanism: Transaction and computations require gas. preventing infinite loops and spam attacks.
+
+4. Determinstic Execution: Given the same input, the EVM always produces the same output across all nodes.
+
+5. Bytecode Execution: Smart contract are compiled into low-level EVM bytecode, which the EVM executes.
+
+
+
+
+
+
+
+
+
+
+
+*/
