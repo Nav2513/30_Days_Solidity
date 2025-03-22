3fac5514549f4031135deaf484198de2b1a69786
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-03-22 22:37:28 +0530

    Adding Day 16 DOS attack

---
+++ src/Day-16-DOS-attack.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+/* 
+
+        --- Denial of Services (DOS) ---
+
+Smart contracts are self-executing programs designed to enforce agreements without intermediaries. Their immutable nature makes security vulnerabilities particularly challenging to address. One potential threat is the Denial of Service (DoS) attack vector, which involves exploiting the contract’s logic to disrupt either the whole protocol’s functionality or at least render some parts of the protocol unusable for other users. DoS attacks can lead to financial losses or disrupt a protocol’s normal working flow.
+
+In traditional network security, a DoS attack overwhelms a machine or network resource with excessive traffic, making it unavailable to intended users. In other words, the attacker attempts to make a computer or network resource unavailable to its intended users by disrupting the services of a host connected to the Internet. The goal of a DoS attack is to overwhelm the targeted system with an enormous amount of requests or data, causing it to become unresponsive or crash.
+
+
+*/
